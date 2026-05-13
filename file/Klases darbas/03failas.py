788ce9c2bd085946ce5f4d33abd9c2b2423748d7
AudriusZ <audriuszaveckas985@gmail.com>
2026-05-13 21:02:14 +0300

    Delete file directory

--- file/Klases darbas/03failas.py
+++
@@ -1,9 +0,0 @@
-with open('03data.txt', 'r', encoding='utf-8') as f:
-    # viskas = f.read()
-    # print(viskas)
-    # print(type(viskas))
-    # a = f.readline()
-    # b = f.readable()
-    f.seek(0) #nukeliaujam i failo pradzia
-    a = f.readlines()
-    print(a)
--- file/Klases darbas/04failas.py
+++
@@ -1,9 +0,0 @@
-with open('04data.txt', 'r', encoding='utf-8') as f:
-    sar1 = [int(x) for x in f.readline().split()]
-    # sar2 = [int(x) for x in f.readline().split()]
-    sar2 = []
-    eilute = f.readline()
-    for x in eilute.split():
-        sar2.append(int(x))
-
-print(sar2)
