a6eb59a285d2e06c76be3fc3f93d1a41f1e687ae
AudriusZ <audriuszaveckas985@gmail.com>
2026-05-13 20:52:46 +0300

    Delete Darbai directory

--- Darbai/1uzd.py
+++
@@ -1,8 +0,0 @@
-with open('tekstas.txt', 'r', encoding='utf-8') as duom:
-    txt = duom.read()
-    sar = txt.split(' ')
-    ilgiausias = ''
-    for i in sar:
-        if len(i) > len(ilgiausias):
-            ilgiausias = i
-print(ilgiausias)
